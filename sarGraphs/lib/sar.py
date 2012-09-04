a9024a7a812ae8183832e40f7e18d0d54d81740f
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-09-04 17:20:32 -0500

    more work on charts

--- sarGraphs/lib/sar.py
+++ sarGraphs/lib/sar.py
@@ -1,8 +1,16 @@
 from pySar.lib import sar
+import os
 
 def get_cpu(key):
     'Get usage for a specific key in cpu report'
     results = sar(sarbin='sar')
+    items = [ float(i[key]) for i in results ]
+    times = shorttimes(results)
+    return zip(times, items)
+    
+def get_load(key):
+    'Get usage for a specific key in cpu report'
+    results = sar(sarbin='sar', saroptions='-q')
     items = [ float(i[key]) for i in results ]
     times = shorttimes(results)
     return zip(times, items)
--- sarGraphs/views.py
+++ sarGraphs/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import TemplateView
-from sarGraphs.lib.sar import get_cpu
+from sarGraphs.lib.sar import get_cpu, get_load
 
 class HomeView(TemplateView):
     'Home Page View'
@@ -7,6 +7,6 @@ class HomeView(TemplateView):
     template_name = "home.html"
     def get_context_data(self, **kwargs):
         context = {}
-        r = get_cpu('idle')
-        context['data'] = r
+        context['cpu'] = get_cpu('%idle')
+        context['load'] = get_load('ldavg-5')
         return context 
